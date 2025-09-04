7574df9acb83fdad066734208aadd201347a96eb
Veer Harischandrakar <1012411025@despu.edu.in>
2025-09-04 23:00:36 +0530

    Update Assignment1.cpp

--- c++/Assignment1.cpp
+++ c++/Assignment1.cpp
@@ -1,55 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[],int n){
-
-    cout << "Running Bubble sort " << endl;
-
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
-}
 
 
-void insertionSort(int arr[],int n){
 
-    cout << "Running Insertion sort " << endl;
 
-    for(int i=1;i<=n-1;i++){
-        int k = arr[i];
-        int j = i-1;
-        
-        while(j>=0 && arr[j]>k){
-            arr[j+1] = arr[j];
-            j--;
-        }
-        
-        arr[j+1] = k;
-    }
-}
 
-void selectionSort(int arr[],int n){
-    cout << "Running Selection sort " << endl;
-
-    for(int i=0;i<n-1;i++){
-        int minIndex = i;
-
-        for(int j=i+1;j<n;j++){
-            if(arr[j] < arr[minIndex]){
-                minIndex = j;
-            }
-        }
-        int temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
-    }
-}
 
 int partition(int arr[],int low,int high){
     int pivot = arr[low];
