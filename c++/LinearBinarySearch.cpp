13adf290fb888f1cdf5fc0d2d6952a26014a5aa8
Veer Harischandrakar <1012411025@despu.edu.in>
2025-09-25 10:00:29 +0530

    Refactor search functions and improve output messages

--- c++/LinearBinarySearch.cpp
+++ c++/LinearBinarySearch.cpp
@@ -1,48 +1,84 @@
 #include <iostream>
 using namespace std;
 
-int LinearSearch(int arr[],int size,int element){
+int linearSearch(int arr[],int size,int element){
     for(int i=0;i<size;i++){
         if(arr[i] == element){
+            cout << "Linear Search: The element " << element << " was found out at index " << i << endl;
             return i;
         }
     }
+    cout << "The element was not found in the array!" << endl;
     return -1;
 }
 
-int binarySearch(int arr[],int size,int element){
-    int low,mid,high;
+void bubbleSort(int arr[],int size){
+    for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-i-1;j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp; 
+            }
+        }
+    }
+}
 
-    low = 0;
-    high = size - 1;
+int binarySearch(int arr[],int size,int element){
+    int low=0,mid,high=size-1;
 
     while(low<=high){
-        mid = (low + high)/2;
-
-        if(arr[mid] == element){
+        mid = (low+high)/2;
+        if(arr[mid] == element ){
             return mid;
         }
-        if(arr[mid] < element){
+        else if(arr[mid] < element){
             low = mid+1;
-        } else{
+        }
+        else{
             high = mid-1;
         }
     }
     return -1;
 }
 
-int main(){ 
+int main(){
+    int size,element;
 
-    int arr[] = {3,4,5,6,7,8,9,10,11,24,31};
-    int size = sizeof(arr)/sizeof(int);
-    int element;
+    cout << "Enter the size of Array: ";
+    cin >> size;
+
+    int arr[size];
+
+    cout << "Enter elements: " << endl;
+    for(int i=0;i<size;i++){
+        cout << i+1 << ": ";
+        cin >> arr[i];
+    }
 
     cout << "Enter the element you want to search for: ";
     cin >> element;
+
+    linearSearch(arr,size,element);
+
+    bubbleSort(arr,size);
+
+    cout << "Sorted array: " << endl;
+    for(int i=0;i<size;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     int index = binarySearch(arr,size,element);
 
-    cout << "The element " << element << " was found at index " << index;
+    if(index != -1){
+        cout << "Binary Search: The element " << element << " was found out at index " << index << endl;
+
+    }
+    else{
+        cout << "The element was not found!" << endl;
+    }
 
     return 0;
-
 }
+
